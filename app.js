7c346722facf44e9c515bfca5be927bb071c3d62
mrtbsc <53538432+mrtbsc@users.noreply.github.com>
2021-05-26 10:42:12 +0200

    simplifying the way info is extracted from the APIs in the app.js file

--- app.js
+++ app.js
@@ -1,63 +1,71 @@
 const today = new Date();
-const yesterday = new Date(today - 24*3600*1000);
+const yesterday = new Date(today - 24 * 3600 * 1000);
 
+$("#searchForm").on("submit", async function (e) {
+  e.preventDefault();
 
-$('#searchForm').on('submit', async function (e) {
-    e.preventDefault();
+  // 1. Get city/town inputed
+  STATUS.targetPlace = DOM.getInputedPlace();
+  DOM.emptyPlaceInput();
 
-    // 1. Get city/town inputed
-    STATUS.targetPlace = DOM.getInputedPlace();
-    DOM.emptyPlaceInput();
+  //2. Get its coordinates
+  const requestConfig = { params: { location: STATUS.targetPlace } };
+  let response = await axios.get(
+    "http://open.mapquestapi.com/geocoding/v1/address?key=" + keys.MQ,
+    requestConfig
+  );
+  STATUS.coordinates = UTILS.extractCoordinates(response);
 
-    //2. Get its coordinates
-    const requestConfig = { params: { location: STATUS.targetPlace } }
-    let response = await axios.get('http://open.mapquestapi.com/geocoding/v1/address?key='+ keys.MQ , requestConfig);
-    STATUS.coordinates = UTILS.extractCoordinates(response);
+  // With the assimilated coordinates, get the place it corresponds to, and print it
+  // SO THE USER CAN DOUBLE CHECK it results in the same place they inputed)
+  let requestConfig2 = {
+    params: { lat: STATUS.coordinates.lat, lon: STATUS.coordinates.lng },
+  };
+  axios
+    .get(
+      "http://open.mapquestapi.com/nominatim/v1/reverse.php?format=json&key=" +
+        keys.MQ,
+      requestConfig2
+    )
+    .then((response) => {
+      STATUS.assimilatedPlace = UTILS.extractPlace(response);
+      DOM.showFoundPlace(STATUS.assimilatedPlace);
+    });
 
-    // With the assimilated coordinates, get the place it corresponds to, and print it 
-    // SO THE USER CAN DOUBLE CHECK it results in the same place they inputed)
-    let requestConfig2 = { params: { lat: STATUS.coordinates.lat ,lon: STATUS.coordinates.lng}}
-    axios.get('http://open.mapquestapi.com/nominatim/v1/reverse.php?format=json&key='+ keys.MQ, requestConfig2)
-        .then( response => {
-            STATUS.assimilatedPlace = UTILS.extractPlace(response);
-            DOM.showFoundPlace(STATUS.assimilatedPlace);
-        })
+  //3. Get the forecasts for those coordinates (for next 48h + for next 7 days)
+  let { data } = await axios.get(
+    "https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,alerts,current&units=metric&appid=" +
+      keys.OW,
+    requestConfig2
+  );
 
-    //3. Get the forecasts for those coordinates (for next 48h + for next 7 days)
-    let { data } = await axios.get('https://api.openweathermap.org/data/2.5/onecall?exclude=minutely,alerts,current&units=metric&appid='+ keys.OW, requestConfig2);
+  let forecasts = UTILS.extractFilteredForecasts(data);
+  STATUS.todaysForecast = forecasts.todaysForecast;
+  STATUS.tomorrowsForecast = forecasts.tomorrowsForecast;
+  STATUS.nextDaysForecast = forecasts.nextDaysForecast;
 
-        //3a. Extract forecasts and filter them 
-        // from redundant or unwanted timestamps 
-        let { hourlyData, dailyData } = UTILS.extractForecasts( data );
-        let { todaysData, tomorrowsData } = UTILS.ommitUnwantedHours(hourlyData);
-        dailyData = UTILS.ommitRedundantDays(dailyData);
+  //4. Get the weather from the day before
+  // (incase they alert possible wet floor)
+  requestConfig2.params.dt = UTILS.jsToOpenWeather(yesterday);
+  response = await axios.get(
+    "https://api.openweathermap.org/data/2.5/onecall/timemachine?units=metric&appid=" +
+      keys.OW,
+    requestConfig2
+  );
+  STATUS.pastWeather = UTILS.extractFilteredWeather(response);
+  STATUS.wasItRainny = UTILS.getPastRain(STATUS.pastWeather);
 
-        //3b. For each wanted timestamp, filter the unwanted info
-        STATUS.todaysForecast = UTILS.extractWantedInfo(todaysData, { isHourly : true });
-        STATUS.tomorrowsForecast = UTILS.extractWantedInfo(tomorrowsData, { isHourly : true });
-        STATUS.nextDaysForecast = UTILS.extractWantedInfo( dailyData );
+  //6. Exhibit hidden forecastSlots or clear them from previous displays
+  DOM.resetSlots();
 
-    //4. Get the weather from the day before 
-    // (incase they alert possible wet floor)
-    requestConfig2.params.dt = UTILS.jsToOpenWeather(yesterday);
-    response = await axios.get('https://api.openweathermap.org/data/2.5/onecall/timemachine?units=metric&appid=' + keys.OW, requestConfig2 );
-    let pastData =  UTILS.extractPastWeather(response);
-    STATUS.pastWeather = UTILS.extractWantedInfo(pastData, { isPast : true });
+  //7. Display current forecasts
+  DOM.display(STATUS.todaysForecast, { isToday: true });
+  DOM.display(STATUS.tomorrowsForecast, { isTomorrow: true });
+  DOM.display(STATUS.nextDaysForecast, { isNextDays: true });
 
-    //6. Exhibit hidden forecastSlots or clear them from previous displays   
-    DOM.resetSlots();
-
-    //7. Display current forecasts
-    DOM.display(STATUS.todaysForecast, { isToday : true });
-    DOM.display(STATUS.tomorrowsForecast, { isTomorrow : true });
-    DOM.display(STATUS.nextDaysForecast, { isNextDays : true });
-
-    //8. Display past weather if it has rained recently
-    STATUS.wasItRainny = UTILS.getPastRain(STATUS.pastWeather);
-    if ( STATUS.wasItRainny ){
-        DOM.showPastAlert();
-        DOM.display(STATUS.nextDaysForecast, { isPast : true });
-    }
-
-})
-
+  //8. Display past weather if it has rained recently
+  if (STATUS.wasItRainny) {
+    DOM.showPastAlert();
+    DOM.display(STATUS.nextDaysForecast, { isPast: true });
+  }
+});
--- utils.js
+++ utils.js
@@ -1,84 +1,114 @@
-
 const UTILS = {
+  /** FORMAT-CONVERSIONS TO INTEGRATE WITH GEOCODING API **/
 
-    /** FORMAT-CONVERSIONS TO INTEGRATE WITH GEOCODING API **/
-        
-        // Extracts the coordinates from the GEOcoding API's response
-        extractCoordinates : function (response) {
-            return response.data.results[0].locations[0].latLng;
-        },
+  // Extracts the coordinates from the GEOcoding API's response
+  extractCoordinates: function (response) {
+    return response.data.results[0].locations[0].latLng;
+  },
 
-        // Extracts the place from the GEOcoding API's response
-        extractPlace : function ( response ) {
-            let { data : { address } } = response;
-            let settlement = (address.city||address.village||address.town);
-            return `${settlement}, ${address.country}`;
-        },
+  // Extracts the place from the GEOcoding API's response
+  extractPlace: function (response) {
+    let {
+      data: { address },
+    } = response;
+    let settlement = address.city || address.village || address.town;
+    return `${settlement}, ${address.country}`;
+  },
 
-    /** FORMAT-CONVERSIONS TO INTEGRATE WITH WEATHER API + DATA FILTERING **/
-        
-        // Converts a JS date to the OpenWeather API's format, and vice versa.
-        jsToOpenWeather : function (date) { 
-            return Math.floor(date.getTime()/1000) 
-        },
-        openWeatherToJs : function (OWDate) { 
-            return new Date(OWDate*1000) 
-        },
+  /** FORMAT-CONVERSIONS TO INTEGRATE WITH WEATHER API **/
 
-        // Extracts forecasts from OpenWeather API's response
-        extractForecasts : function ( object ) {
-            let hourlyData = object.hourly;
-            let dailyData = object.daily;
-            return { hourlyData, dailyData };
-        },
+  // Extracts forecasts from OpenWeather API's response
+  extractForecasts: function (object) {
+    let hourlyData = object.hourly;
+    let dailyData = object.daily;
+    return { hourlyData, dailyData };
+  },
 
-        extractPastWeather : function ( response ) {
-            return response.data.hourly;
-        },
+  // Extracts past weather from OpenWeather API's response
+  extractPastWeather: function (response) {
+    return response.data.hourly;
+  },
 
-        ommitUnwantedHours : function (array) {
-            //let's separate the hours from the current day and the next 
-            //(and ignore the day after that, for which we might not have all hours)
-            
-            let hoursDayAfterTomorrow = today.getHours();
-            let todaysData = array.slice(1 , 24 - hoursDayAfterTomorrow);
-            let tomorrowsData = array.slice( 24 - hoursDayAfterTomorrow, 48 - hoursDayAfterTomorrow);
-            return { todaysData, tomorrowsData };
-        }, 
+  // Converts a JS date to the OpenWeather API's format, and vice versa.
+  jsToOpenWeather: function (date) {
+    return Math.floor(date.getTime() / 1000);
+  },
+  openWeatherToJs: function (OWDate) {
+    return new Date(OWDate * 1000);
+  },
 
-        ommitRedundantDays : function (array) {
-            //let's ignore the daily forecast of the 1st and 2nd days 
-            //(for which we display the hourly forecasts)    
-            return array.slice(2,7);
-        },
+  /** FILTERING THE DATA FROM THE WEATHER API'S RESPONSES**/
 
-        // Filter out info that is not needed for every timestamp
-        extractWantedInfo : function (array,  configuration = { isHourly : false , isPast : false }) {
-            let { isHourly , isPast } = configuration;
-            
-            return array.map( function( interval ) {
-                let recievedMms = (isPast && interval.rain) ?
-                    interval.rain["1h"] : 0 ;
-                return {
-                    date : new Date(1000*interval.dt),
-                    degrees : (isHourly || isPast) ? Math.round(interval.temp) : Math.round((interval.temp.min + interval.temp.max)/2),
-                    iconPath : `./images/${interval.weather[0].icon}.png`,
-                    rainProb : isPast? null : Math.round(interval.pop*10)*10,
-                    mmsOfRain : isPast? recievedMms : null,
-                    weatherId : interval.weather[0].id
-          
-                };
-            });
-        },
+  // Extract wanted forecast info
+  extractFilteredForecasts: function (data) {
+    // Extract forecasts
+    let { hourlyData, dailyData } = UTILS.extractForecasts(data);
 
-        getPastRain : function ( hours ) {
-            return hours.reduce( (accu, hour) => (accu + hour.mmsOfRain), 0);
-        },
+    // filter out redundant or unwanted timestamps
+    let { todaysData, tomorrowsData } = this.ommitUnwantedHours(hourlyData);
+    dailyData = this.ommitRedundantDays(dailyData);
 
-        // // Checks if a weather id involves any precipitation (based on the id)
-        // hasPrecipitation : function ( hour ) {
-        //     let id = hour.weatherId;
-        //     return (id >= 200) && (id <= 622) &&  ![210, 211, 212, 221].includes(id);       
-        // }
+    // filter out unwanted info, for each wanted timestamp
+    let todaysForecast = this.ommitUnwantedInfo(todaysData, { isHourly: true });
+    let tomorrowsForecast = this.ommitUnwantedInfo(tomorrowsData, {
+      isHourly: true,
+    });
+    let nextDaysForecast = this.ommitUnwantedInfo(dailyData);
+    return { todaysForecast, tomorrowsForecast, nextDaysForecast };
+  },
 
-}
+  getPastRain: function (hours) {
+    return hours.reduce((accu, hour) => accu + hour.mmsOfRain, 0);
+  },
+
+  //Extract wanted past weather info
+  extractFilteredWeather: function (response) {
+    pastData = this.extractPastWeather(response);
+    let pastWeather = this.ommitUnwantedInfo(pastData, {
+      isPast: true,
+    });
+    return pastWeather;
+  },
+
+  ommitUnwantedHours: function (array) {
+    //let's separate the hours from the current day and the next
+    //(and ignore the day after that, for which we might not have all hours)
+
+    let hoursDayAfterTomorrow = today.getHours();
+    let todaysData = array.slice(1, 24 - hoursDayAfterTomorrow);
+    let tomorrowsData = array.slice(
+      24 - hoursDayAfterTomorrow,
+      48 - hoursDayAfterTomorrow
+    );
+    return { todaysData, tomorrowsData };
+  },
+
+  ommitRedundantDays: function (array) {
+    //let's ignore the daily forecast of the 1st and 2nd days
+    //(for which we display the hourly forecasts)
+    return array.slice(2, 7);
+  },
+
+  // Filter out info that is not needed for every timestamp
+  ommitUnwantedInfo: function (
+    array,
+    configuration = { isHourly: false, isPast: false }
+  ) {
+    let { isHourly, isPast } = configuration;
+
+    return array.map(function (interval) {
+      let recievedMms = isPast && interval.rain ? interval.rain["1h"] : 0;
+      return {
+        date: new Date(1000 * interval.dt),
+        degrees:
+          isHourly || isPast
+            ? Math.round(interval.temp)
+            : Math.round((interval.temp.min + interval.temp.max) / 2),
+        iconPath: `./images/${interval.weather[0].icon}.png`,
+        rainProb: isPast ? null : Math.round(interval.pop * 10) * 10,
+        mmsOfRain: isPast ? recievedMms : null,
+        weatherId: interval.weather[0].id,
+      };
+    });
+  },
+};
